aae9e3068a55030f8707cb2e3eda48e7c3d9974d
sourcecoursepoc <sourcecourse.poc@gmail.com>
2023-03-15 17:05:55 +0530

    updated schemas

--- src/pages/schemas/index.tsx
+++ src/pages/schemas/index.tsx
@@ -1,7 +1,22 @@
-const Schemas = () => {
-    return (
-        <h1>Schema page</h1>
-    )
-}
+import { Layout, Space, Col, Row } from "antd";
+import Header from "../../components/header/header";
+import styles from "./schemas.module.css";
 
-export default Schemas
+const Schemas = () => {
+  const { Content } = Layout;
+  return (
+    <Space direction="vertical" className={styles.space} size={[0, 48]}>
+      <Layout className={styles.layout}>
+        <Header />
+        <Content>
+          <Row>
+            <Col span={8}>Left</Col>
+            <Col span={16}>Right</Col>
+          </Row>
+        </Content>
+      </Layout>
+    </Space>
+  );
+};
+
+export default Schemas;
