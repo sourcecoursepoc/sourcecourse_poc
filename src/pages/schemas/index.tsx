a6e723ed92491f477c6aa7e1e819198cf51ce30f
Lakke Rajashekar(UST <211458@ust-global.com>
2023-03-16 18:12:13 +0530

    basic treeview is done and soem error is there is icon imports.

--- src/pages/schemas/index.tsx
+++ src/pages/schemas/index.tsx
@@ -1,6 +1,10 @@
-import { Layout, Space, Col, Row } from "antd";
+import { Layout, Space, Col, Row, Menu } from "antd";
+import { CloseOutlined, PlusOutlined } from '@ant-design/icons';
 import Header from "../../components/header/header";
 import styles from "./schemas.module.css";
+import { Item } from "rc-menu";
+import TreeView from "./TreeView";
+
 
 const Schemas = () => {
   const { Content } = Layout;
@@ -8,9 +12,27 @@ const Schemas = () => {
     <Space direction="vertical" className={styles.space} size={[0, 48]}>
       <Layout className={styles.layout}>
         <Header />
+        <Menu className={styles.menu}>
+          <Row>
+            <Col className={styles.rows}>
+              <Item key="add" ><PlusOutlined /></Item>
+            </Col>
+            <Col className={styles.rows}>
+              <Item key="icon">icon</Item>
+            </Col>
+            <Col className={styles.rows}>
+              <Item key="icon">icon</Item>
+            </Col>
+            <Col>
+              <Item key="close">
+                <CloseOutlined />
+              </Item>
+            </Col>
+          </Row>
+        </Menu>
         <Content>
           <Row>
-            <Col span={8}>Left</Col>
+            <Col span={6} className={styles.treeview}><TreeView /></Col>
             <Col span={16}>Right</Col>
           </Row>
         </Content>
