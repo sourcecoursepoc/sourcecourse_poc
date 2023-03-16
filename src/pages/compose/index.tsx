cd3d564e29fbc2d08861d178267c8e9ae4457cd5
sourcecoursepoc <sourcecourse.poc@gmail.com>
2023-03-16 16:15:18 +0530

    added compose

--- src/pages/compose/index.tsx
+++ src/pages/compose/index.tsx
@@ -1,9 +1,26 @@
+import { Layout, Space, Col, Row } from "antd";
+import Header from "../../components/header/header";
+import styles from "./compose.module.css";
+import { Divider } from "antd";
 
 const Compose = () => {
-   
-    return (
-        <h1>Compose page</h1>
-    )
-}
+  const { Content } = Layout;
+  return (
+    <Space direction="vertical" className={styles.space} size={[0, 48]}>
+      <Layout className={styles.layout}>
+        <Header />
+        <Content>
+          <Row>
+            <Col span={24}>Top</Col>
+          </Row>
+          <Divider plain></Divider>
+          <Row>
+            <Col span={24}>Bottom</Col>
+          </Row>
+        </Content>
+      </Layout>
+    </Space>
+  );
+};
 
-export default Compose
+export default Compose;
