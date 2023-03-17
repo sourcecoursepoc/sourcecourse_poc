e98098af656968ccc40ad2e9ecfebd37a90116a5
sourcecoursepoc <sourcecourse.poc@gmail.com>
2023-03-17 12:56:56 +0530

    changes added

--- src/pages/schemas/index.tsx
+++ src/pages/schemas/index.tsx
@@ -1,18 +1,20 @@
 import { Layout, Space, Col, Row } from "antd";
 import Header from "../../components/header/header";
 import styles from "./schemas.module.css";
-import TreeView from "./TreeView";
-import Schemamenu from "./schemamenu";
+import TreeView from "./treeview";
+import SchemaMenu from "./schemamenu";
 const Schemas = () => {
   const { Content } = Layout;
   return (
     <Space direction="vertical" className={styles.space} size={[0, 48]}>
       <Layout className={styles.layout}>
         <Header />
-        <Schemamenu/>
+        <SchemaMenu />
         <Content>
           <Row>
-            <Col span={6} className={styles.treeview}><TreeView /></Col>
+            <Col span={6} className={styles.treeview}>
+              <TreeView />
+            </Col>
             <Col span={16}>Right</Col>
           </Row>
         </Content>
