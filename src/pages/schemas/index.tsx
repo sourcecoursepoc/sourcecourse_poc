b4d2e71c75b2135049c2eece7550b0e3aebd0869
Lakke Rajashekar(UST <211458@ust-global.com>
2023-03-17 11:19:09 +0530

    schemas menu changes as seperate component

--- src/pages/schemas/index.tsx
+++ src/pages/schemas/index.tsx
@@ -1,35 +1,15 @@
-import { Layout, Space, Col, Row, Menu } from "antd";
-import { CloseOutlined, PlusOutlined } from '@ant-design/icons';
+import { Layout, Space, Col, Row } from "antd";
 import Header from "../../components/header/header";
 import styles from "./schemas.module.css";
-import { Item } from "rc-menu";
 import TreeView from "./TreeView";
-
-
+import Schemamenu from "./schemamenu";
 const Schemas = () => {
   const { Content } = Layout;
   return (
     <Space direction="vertical" className={styles.space} size={[0, 48]}>
       <Layout className={styles.layout}>
         <Header />
-        <Menu className={styles.menu}>
-          <Row>
-            <Col className={styles.rows}>
-              <Item key="add" ><PlusOutlined /></Item>
-            </Col>
-            <Col className={styles.rows}>
-              <Item key="icon">icon</Item>
-            </Col>
-            <Col className={styles.rows}>
-              <Item key="icon">icon</Item>
-            </Col>
-            <Col>
-              <Item key="close">
-                <CloseOutlined />
-              </Item>
-            </Col>
-          </Row>
-        </Menu>
+        <Schemamenu/>
         <Content>
           <Row>
             <Col span={6} className={styles.treeview}><TreeView /></Col>
---
+++ src/pages/schemas/schemamenu.tsx
@@ -0,0 +1,28 @@
+import { Menu, Row, Col } from 'antd';
+import { Item } from 'rc-menu';
+import styles from "./schemas.module.css";
+import { CloseOutlined, PlusOutlined } from '@ant-design/icons';
+// const { SubMenu } = Menu;
+export default function Schemamenu() {
+    return (
+        <Menu className={styles.menu}>
+            <Row>
+                <Col className={styles.rows}>
+                    <Item key="add" ><PlusOutlined/></Item>
+                </Col>
+                <Col className={styles.rows}>
+                    <Item key="icon">icon</Item>
+                </Col>
+                <Col className={styles.rows}>
+                    <Item key="icon">icon</Item>
+                </Col>
+                <Col className={styles.closebutton}>
+                    <Item key="close">
+                        <CloseOutlined />
+                    </Item>
+                </Col>
+            </Row>
+        </Menu>
+
+    );
+}
