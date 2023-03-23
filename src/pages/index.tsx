237e6604b3573b03ed4dd0e2595b75dfa10cc43a
sourcecoursepoc <sourcecourse.poc@gmail.com>
2023-03-23 10:29:00 +0530

    removed axios from home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,13 +9,6 @@ import { useEffect } from "react";
 
 const Home: React.FunctionComponent = () => {
   const { Content } = Layout;
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:8000/schemas")
-      .then((res) => console.log(res))
-      .catch((error) => console.log(error));
-  });
   return (
     <Space direction="vertical" className={styles.space} size={[0, 48]}>
       <Layout className={styles.layout}>
