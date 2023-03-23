186e76febe63d5283b88abecd58f9fd14ee788d4
212040-AnnMariaJenson <212040@ust-global.com>
2023-03-23 09:31:59 +0530

    Design of Pipeline Queue is done

--- src/pages/schemas/displaybox.tsx
+++ src/pages/schemas/displaybox.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import { Card, Row, Col, Divider } from 'antd';
+import React from "react";
+import { Card, Row, Col, Divider } from "antd";
 import styles from "./displaybox.module.css";
 import "./content.module.css";
 const DisplayBox = () => (
   <>
-      <div className={styles.outerbox}>
-        <div className={styles.innerright}>
-          <Row className={styles.image}
-          ><Col>
-            image
-            </Col>
+    <div className={styles.outerbox}>
+      <div className={styles.innerright}>
+        <Row className={styles.image}>
+          <Col>image</Col>
         </Row>
-          <Row className={styles.imageName}>
-          <Col>schema</Col></Row>
-        </div>
-        <div className={styles.innerleft}>
-          <Row className={styles.items}><Col><p>items</p></Col></Row>
-        </div>
+        <Row className={styles.imageName}>
+          <Col>schema</Col>
+        </Row>
       </div>
+      <div className={styles.innerleft}>
+        <Row className={styles.items}>
+          <Col>
+            <p>items</p>
+          </Col>
+        </Row>
+      </div>
+    </div>
   </>
 );
 
 export default DisplayBox;
-
-
-
