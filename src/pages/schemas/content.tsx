7b2af59667f1991ccba584c299a2243487999d49
Lakke Rajashekar(UST <211458@ust-global.com>
2023-03-24 11:03:59 +0530

    display boxes changed

--- src/pages/schemas/content.tsx
+++ src/pages/schemas/content.tsx
@@ -4,16 +4,18 @@ import styles from "./content.module.css";
 import DescriptionBox from './descriptionbox';
 import DisplayBox from './displaybox';
 import TagBox from './tagbox';
+import { DatabaseOutlined, ApartmentOutlined, SyncOutlined } from '@ant-design/icons';
 const { Content } = Layout;
+
 export default function SchemaContent() {
     return (
         <Layout className={styles.layout}>
             <Content className={styles.content}>
                 <Row className={styles.pinkbar} />
                 <Row gutter={[16, 16]}>
-                    <DisplayBox />
-                    <DisplayBox />
-                    <DisplayBox />
+                    <DisplayBox text={"IN_WT_TABLES"} iconName={"server"} icon={<DatabaseOutlined />}/>
+                    <DisplayBox text={"8"} iconName={"schemas"} icon={<ApartmentOutlined />}/>
+                    <DisplayBox text={"ONLINE"} iconName={"status"} icon={<SyncOutlined />}/>
                 </Row>
                 <Row className={styles.descriptionbox}>
                     <DescriptionBox />
--- src/pages/schemas/descriptionbox.tsx
+++ src/pages/schemas/descriptionbox.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { Input } from 'antd';
+import { Input, Row, Col } from 'antd';
 const { TextArea } = Input;
 const DescriptionBox: React.FC = () => (
     <>
         <TextArea placeholder="Description" />
+        {/* <Row style={{border:"1px solid", borderRadius:"5px",textAlign:"left"}}>
+            <Col>
+                <p >
+             Aenean blandit vulputate tellus a dapibus. Phasellus volutpat turpis quis massa laoreet blandit. Integer pulvinar ligula at massa auctor, vel vehicula diam malesuada. In malesuada laoreet tellus, et vestibulum velit congue quis. In viverra neque at massa accumsan, id ornare urna bibendum. Quisque tempor vitae sem sed varius. Curabitur leo leo, scelerisque vitae lorem porttitor, efficitur scelerisque nulla. Praesent et luctus sapien. Aliquam mollis vel nisi bibendum pulvinar. Mauris scelerisque ex in enim malesuada, non faucibus mi laoreet. Etiam aliquam est non lectus dictum fermentum. In suscipit nunc id dolor aliquam porta. Morbi auctor efficitur ante, sed tristique mi interdum ultrices. Donec tincidunt sem sit amet accumsan sollicitudin. Praesent in dolor in enim imperdiet venenatis euismod vel urna
+          </p></Col>
+        </Row> */}
     </>
 );
 
