c283d5a3ee7a40d587c95a6a1f4e55c0c539a7ac
212040-AnnMariaJenson <212040@ust-global.com>
2023-03-24 09:46:08 +0530

    conflicts resolved

--- src/components/homeRightArea/homeRightArea.tsx
+++ src/components/homeRightArea/homeRightArea.tsx
@@ -6,6 +6,7 @@ import {
   ClusterOutlined,
   CheckCircleOutlined,
   CloseCircleOutlined,
+  DatabaseOutlined,
 } from "@ant-design/icons";
 import { getPipelineSelector } from "../../redux/selectors";
 import { useSelector, useDispatch } from "react-redux";
@@ -22,58 +23,6 @@ const HomeRightArea: React.FunctionComponent = () => {
   }, []);
 
   return (
-    // <Layout className={styles.layout}>
-    //   <Content className={styles.content}>
-    //     <Row className={styles.pinkbar}>
-    //       <p className={styles.textStyle}>Pipeline Queue</p>
-    //     </Row>
-    //     <Row
-    //       gutter={[16, 16]}
-    //       className={styles.rowTextStyle}
-    //       align="middle"
-    //       style={{ marginBottom: "20px", marginTop: "10px" }}
-    //     >
-    //       <Col span={4}>
-    //         <ClusterOutlined style={{ fontSize: "26px" }} />
-    //       </Col>
-    //       <Col span={12} style={{ fontSize: "11px" }}>
-    //         Transom Initial Load <br /> 02/21/2023 1:00 Am{" "}
-    //       </Col>
-    //       <Col span={4}>
-    //         <CheckCircleOutlined
-    //           style={{ color: "#52c41a", fontSize: "26px" }}
-    //         />
-    //       </Col>
-    //       <Col span={4} style={{ borderLeft: "1px solid #ccc" }}>
-    //         <Progress type="circle" percent={80} strokeWidth={8} size={26} />
-    //       </Col>
-    //     </Row>
-
-    //     {/* <Row gutter={[16, 16]} className={styles.rowTextStyle} align="middle">
-    //       <Col span={4}>
-    //         <ClusterOutlined style={{ fontSize: "26px" }} />
-    //       </Col>
-    //       <Col span={12} style={{ fontSize: "11px" }}>
-    //         Pricing Daily Sync <br /> 02/21/2023 1:00 Am{" "}
-    //       </Col>
-    //       <Col span={4}>
-    //         <CloseCircleOutlined style={{ color: "red", fontSize: "26px" }} />
-    //       </Col>
-    //       <Col span={4} style={{ borderLeft: "1px solid #ccc" }}>
-    //         <Progress type="circle" percent={0} strokeWidth={8} size={26} />
-    //       </Col>
-    //     </Row> */}
-    //     <Row className={styles.lowerDivider}></Row>
-    //     <Button
-    //       type="link"
-    //       href="/pipeline"
-    //       style={{ color: "#7a63a9", marginLeft: "75%", fontWeight: "500" }}
-    //     >
-    //       View All
-    //     </Button>
-    //   </Content>
-    // </Layout>
-
     <Layout className={styles.layout}>
       <Content className={styles.content}>
         <Row className={styles.pinkbar}>
@@ -88,7 +37,13 @@ const HomeRightArea: React.FunctionComponent = () => {
             style={{ marginBottom: "20px", marginTop: "10px" }}
           >
             <Col span={4}>
-              <ClusterOutlined style={{ fontSize: "26px" }} />
+              {pipeline.type === "Initial Load" ? (
+                <ClusterOutlined style={{ fontSize: "26px", color: "blue" }} />
+              ) : (
+                <DatabaseOutlined
+                  style={{ fontSize: "26px", color: "#AA336A" }}
+                />
+              )}
             </Col>
             <Col span={12} style={{ fontSize: "11px" }}>
               {pipeline.pipelineName} <br /> {pipeline.timeStamp}{" "}
--- src/pages/pipeline/pipelineQueuePage.tsx
+++ src/pages/pipeline/pipelineQueuePage.tsx
@@ -1,12 +1,13 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import styles from "./pipelineQueuePage.module.css";
 import { Layout, Row, Col, Progress, Button } from "antd";
 import {
   ClusterOutlined,
   CheckCircleOutlined,
   CloseCircleOutlined,
+  DatabaseOutlined,
 } from "@ant-design/icons";
-import { useSelector,useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { getPipelineSelector } from "@/redux/selectors";
 import { fetchPipeline } from "@/redux/actions/fetchDataAction";
 const { Content } = Layout;
@@ -18,58 +19,64 @@ const QueuePage: React.FunctionComponent = () => {
   useEffect(() => {
     dispatch(fetchPipeline());
   }, []);
-  
+
   return (
     <Layout className={styles.layout}>
-    <Content className={styles.content}>
-      <Row className={styles.pinkbar}>
-        <p className={styles.textStyle}>Pipeline Queue</p>
-      </Row>
-      {pipelineList.map((pipeline) => (
-        <Row
-          key={pipeline.id}
-          gutter={[16, 16]}
-          className={styles.rowTextStyle}
-          align="middle"
-          style={{ marginBottom: "20px", marginTop: "10px" }}
-        >
-          <Col span={4}>
-            <ClusterOutlined style={{ fontSize: "26px" }} />
-          </Col>
-          <Col span={12} style={{ fontSize: "11px" }}>
-            {pipeline.pipelineName} <br /> {pipeline.timeStamp}{" "}
-          </Col>
-          <Col span={4}>
-            {pipeline.status === true ? (
-              <CheckCircleOutlined
-                style={{ color: "#52c41a", fontSize: "26px" }}
-              />
-            ) : (
-              <CloseCircleOutlined
-                style={{ color: "red", fontSize: "26px" }}
-              />
-            )}
-          </Col>
-          <Col span={4} style={{ borderLeft: "1px solid #ccc" }}>
-            <Progress
-              type="circle"
-              percent={pipeline.statusperecntage}
-              strokeWidth={8}
-              size={26}
-            />
-          </Col>
+      <Content className={styles.content}>
+        <Row className={styles.pinkbar}>
+          <p className={styles.textStyle}>Pipeline Queue</p>
         </Row>
-      ))}
-      <Row className={styles.lowerDivider}></Row>
-      <Button
-        type="link"
-        href="/pipeline"
-        style={{ color: "#7a63a9", marginLeft: "75%", fontWeight: "500" }}
-      >
-        View All
-      </Button>
-    </Content>
-  </Layout>
+        {pipelineList.map((pipeline) => (
+          <Row
+            key={pipeline.id}
+            gutter={[16, 16]}
+            className={styles.rowTextStyle}
+            align="middle"
+            style={{ marginBottom: "20px", marginTop: "10px" }}
+          >
+            <Col span={4}>
+              {pipeline.type === "Initial Load" ? (
+                <ClusterOutlined style={{ fontSize: "26px", color: "blue" }} />
+              ) : (
+                <DatabaseOutlined
+                  style={{ fontSize: "26px", color: "#AA336A" }}
+                />
+              )}
+            </Col>
+            <Col span={12} style={{ fontSize: "11px" }}>
+              {pipeline.pipelineName} <br /> {pipeline.timeStamp}{" "}
+            </Col>
+            <Col span={4}>
+              {pipeline.status === true ? (
+                <CheckCircleOutlined
+                  style={{ color: "#52c41a", fontSize: "26px" }}
+                />
+              ) : (
+                <CloseCircleOutlined
+                  style={{ color: "red", fontSize: "26px" }}
+                />
+              )}
+            </Col>
+            <Col span={4} style={{ borderLeft: "1px solid #ccc" }}>
+              <Progress
+                type="circle"
+                percent={pipeline.statusperecntage}
+                strokeWidth={8}
+                size={26}
+              />
+            </Col>
+          </Row>
+        ))}
+        <Row className={styles.lowerDivider}></Row>
+        <Button
+          type="link"
+          href="/"
+          style={{ color: "#7a63a9", marginLeft: "90%", fontWeight: "500" }}
+        >
+          Home
+        </Button>
+      </Content>
+    </Layout>
   );
 };
 
