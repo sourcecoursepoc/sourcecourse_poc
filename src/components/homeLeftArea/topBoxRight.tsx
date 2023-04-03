07454d0bc300eef997fc825eab48b12cbdfc4d21
swathi-krishna-k <Swathi.Krishna@ust.com>
2023-04-03 14:39:42 +0530

    transcription file added

--- src/components/homeLeftArea/topBoxRight.tsx
+++ src/components/homeLeftArea/topBoxRight.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { Row, Col, Divider, Statistic, Button } from "antd";
+import { Row, Col, Divider, Statistic, Button, Image } from "antd";
 import styles from "./topBoxRight.module.css";
 
 const TopBoxRight: React.FC = () => {
   return (
     <>
       <div className={styles.outerbox}>
-        <div className={styles.innerrightTop}></div>
-
-        <div className={styles.innerrightBottom}>
+        <div className={styles.innerrightTop}>
+          <Image src="./compose.png"  style={{height:"2.5rem",marginTop:"1.1rem",marginLeft:"2.3rem",width:"3.6rem"}}></Image>
+        </div>
+           <div className={styles.innerrightBottom}>
           {" "}
           <Button
             type="link"
