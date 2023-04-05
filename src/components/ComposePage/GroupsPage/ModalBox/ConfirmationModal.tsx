4f51ab4b1d0626cabc1756d9a5f492409086aaee
swathi-krishna-k <Swathi.Krishna@ust.com>
2023-04-05 14:46:32 +0530

    added confirmation box

---
+++ src/components/ComposePage/GroupsPage/ModalBox/ConfirmationModal.tsx
@@ -0,0 +1,41 @@
+import { Button, Modal } from "antd";
+import { ExclamationCircleOutlined } from "@ant-design/icons";
+
+interface ConfirmationModalProps {
+  visible: boolean;
+  title: string;
+  message: string;
+  onOk: () => void;
+  onCancel: () => void;
+}
+
+const ConfirmationModal: React.FC<ConfirmationModalProps> = ({
+  visible,
+  title,
+  message,
+  onOk,
+  onCancel,
+}) => {
+  return (
+    <Modal
+      visible={visible}
+      title={title}
+      onCancel={onCancel}
+      footer={[
+        <Button key="cancel" danger onClick={onCancel}>
+          Cancel
+        </Button>,
+        <Button key="ok" type="primary"  onClick={onOk}>
+          OK
+        </Button>,
+        
+      ]}
+    >
+      <p>
+        {/* <ExclamationCircleOutlined style={{ color: "#FFA500" }} />  */}{message}
+      </p>
+    </Modal>
+  );
+};
+
+export default ConfirmationModal;
--- src/components/ComposePage/GroupsPage/ModalBox/groupsModalBoxButtons.tsx
+++ src/components/ComposePage/GroupsPage/ModalBox/groupsModalBoxButtons.tsx
@@ -1,64 +1,147 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Col, Divider, Input, Modal, Row } from "antd";
 import Buttons from "../../buttons/buttons";
 import styles from "../ModalBox/groupsModalBox.module.css";
-import { CloseOutlined, CopyFilled, DeleteFilled, SaveFilled,BranchesOutlined } from "@ant-design/icons";
+import {
+  CloseOutlined,
+  CopyFilled,
+  DeleteFilled,
+  SaveFilled,
+  BranchesOutlined,
+} from "@ant-design/icons";
 import DeleteButtonModalBox from "./deleteButtonModalBox";
+import ConfirmationModal from "./ConfirmationModal";
 
 const GroupsModalBoxuttons = () => {
-    return (
-        <>
-        <Row style={{ marginLeft: "-1rem", marginRight: "-1rem" }}>
-            <Col span={6} style={{ padding: "0 6px" }}>
-              <Button className={styles.mygroupbutton}
-                icon={<DeleteFilled className={styles.icon} />}
-                size={"middle"}
-                type="primary"
-                shape="round"
-                onClick={() => {}}
-                href={""}
-                
-              >Delete</Button>
-            </Col>
-            <Col span={6} style={{ padding: "0 6px" }}>
-            <Button className={styles.mygroupbutton}
-                icon={<CopyFilled className={styles.icon} />}
-                size={"middle"}
-                type="primary"
-                shape="round"
-                onClick={() => {}}
-                href={""}
-                
-              >Clone</Button>
-            </Col>
-            <Col span={6} style={{ padding: "0 6px" }}>
-            <Button className={styles.mygroupbutton}
-                icon={<SaveFilled className={styles.icon} />}
-                size={"middle"}
-                type="primary"
-                shape="round"
-                onClick={() => {}}
-                href={""}
-                
-              >Save</Button>
-            </Col>
-            <Col span={6} style={{ padding: "0 6px" }}>
-            <Button className={styles.mygroupbutton}
-                icon={<CloseOutlined className={styles.icon} />}
-                size={"middle"}
-                type="primary"
-                shape="round"
-                onClick={() => {}}
-                href={""}
-                
-              >Exit</Button>
-            </Col>
-            <Col span={6} >
-              <Button icon={<BranchesOutlined className={styles.icon} />} className={styles.pipelinebutton}>Create Pipeline</Button>
-            </Col>
-          </Row>
-          </>
-    )
-  }
-  
-  export default GroupsModalBoxuttons
+  const [saveModalVisible, setSaveModalVisible] = useState(false);
+  const [exitModalVisible, setExitModalVisible] = useState(false);
+  const [deleteModalVisible, setDeleteModalVisible] = useState(false);
+  const handleSaveClick = () => {
+    setSaveModalVisible(true);
+  };
+
+  const handleSaveModalOk = () => {
+   setSaveModalVisible(false);
+    
+  };
+
+  const handleSaveModalCancel = () => {
+    setSaveModalVisible(false);
+  }; 
+
+  const handleExitClick = () => {
+    setExitModalVisible(true);
+   
+  };
+
+  const handleExitModalOk = () => {
+    setExitModalVisible(false);
+    window.location.href = "";
+    
+  };
+
+  const handleExitModalCancel = () => {
+    setExitModalVisible(false);
+  }; 
+  const handleDeleteClick = () => {
+    setDeleteModalVisible(true);
+  };
+
+  const handleDeleteModalOk = () => {
+    setDeleteModalVisible(false);
+    
+  };
+
+  const handleDeleteModalCancel = () => {
+    setDeleteModalVisible(false);
+  }; 
+
+  return (
+    <>
+      <Row style={{ marginLeft: "-1rem", marginRight: "-1rem" }}>
+        <Col span={6} style={{ padding: "0 6px" }}>
+          <Button
+            className={styles.mygroupbutton}
+            icon={<DeleteFilled className={styles.icon} />}
+            size={"middle"}
+            type="primary"
+            shape="round"
+            onClick={handleDeleteClick}
+          >
+            Delete
+          </Button>
+          <ConfirmationModal
+            visible={deleteModalVisible}
+            onOk={handleDeleteModalOk}
+            onCancel={handleDeleteModalCancel}
+            title="Delete Confirmation"
+            message="Are you sure you want to Delete?"
+          />
+        </Col>
+        <Col span={6} style={{ padding: "0 6px" }}>
+          <Button
+            className={styles.mygroupbutton}
+            icon={<CopyFilled className={styles.icon} />}
+            size={"middle"}
+            type="primary"
+            shape="round"
+            onClick={() => {}}
+            href={""}
+          >
+            Clone
+          </Button>
+        </Col>
+        <Col span={6} style={{ padding: "0 6px" }}>
+          <Button
+            className={styles.mygroupbutton}
+            icon={<SaveFilled className={styles.icon} />}
+            size={"middle"}
+            type="primary"
+            shape="round"
+            onClick={handleSaveClick}
+            
+          >
+            Save
+          </Button>
+          <ConfirmationModal
+            visible={saveModalVisible}
+            onOk={handleSaveModalOk}
+            onCancel={handleSaveModalCancel}
+            title="Save Confirmation"
+            message="Are you sure you want to save?"
+          />
+        </Col>
+        <Col span={6} style={{ padding: "0 6px" }}>
+          <Button
+            className={styles.mygroupbutton}
+            icon={<CloseOutlined className={styles.icon} />}
+            size={"middle"}
+            type="primary"
+            shape="round"
+            onClick={handleExitClick}
+          
+          >
+            Exit
+          </Button>
+          <ConfirmationModal
+            visible={exitModalVisible}
+            onOk={handleExitModalOk}
+            onCancel={handleExitModalCancel}
+            title="Exit Confirmation"
+            message="Are you sure you want to Exit?"
+          />
+        </Col>
+        <Col span={6}>
+          <Button
+            icon={<BranchesOutlined className={styles.icon} />}
+            className={styles.pipelinebutton}
+          >
+            Create Pipeline
+          </Button>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default GroupsModalBoxuttons;
