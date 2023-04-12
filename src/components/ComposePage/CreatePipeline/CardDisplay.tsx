90f2f00898acdb37f46e7951c5aacff15782712a
pavith-sudhir <pavith.sudhir@ust.com>
2023-04-12 17:05:02 +0530

    changes in create pipeline

--- src/components/ComposePage/CreatePipeline/CardDisplay.tsx
+++ src/components/ComposePage/CreatePipeline/CardDisplay.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const CardDisplay = ({alt,src,title}) => {
     return (
-        <Card hoverable style={{ width: 240 }}>
+        <Card hoverable style={{ width: 240, margin: "0.2rem" }}>
             <Card.Meta
                 avatar={<img height={30} alt={alt} src={src} style={{ marginRight: 10 }} />}
                 title={title}
