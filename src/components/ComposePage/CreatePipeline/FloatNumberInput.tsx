002225c82e3a587ffb722886c35cf6d61eb4e12e
swathi-krishna-k <Swathi.Krishna@ust.com>
2023-05-24 15:08:12 +0530

    Fixed all errors in FloatNumberInuput File

--- src/components/ComposePage/CreatePipeline/FloatNumberInput.tsx
+++ src/components/ComposePage/CreatePipeline/FloatNumberInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState ,ChangeEvent } from "react";
+import React, { useState, ChangeEvent } from "react";
 import { Input, InputNumber } from "antd";
 
 import styles from "../../../styles/floatInput.module.css";
@@ -8,53 +8,47 @@ interface FloatNumberInputProps {
   type: string;
   required?: boolean;
   dataValue?: number;
-  onChange: (value: number) => void;
+  onChange: (value: number | null) => void;
   min?: number;
   max?: number;
 }
-const FloatNumberInput: React.FC<FloatNumberInputProps> =(props) => {
+const FloatNumberInput: React.FC<FloatNumberInputProps> = (props) => {
   const [focus, setFocus] = useState(false);
-  let { label, placeholder, type, required,dataValue } = props;
-
-  const [value, setValue] = useState('');
-
-  // if (!placeholder) placeholder = label;
-
+  let { label, placeholder, type, required, dataValue } = props;
+  const [value, setValue] = useState("");
   const isOccupied = focus || (value && value.length !== 0);
-
-  const labelClass = isOccupied ? `${styles.label} ${styles.aslabel}` : `${styles.label} ${styles.asplaceholder}`
-
+  const labelClass = isOccupied
+    ? `${styles.label} ${styles.aslabel}`
+    : `${styles.label} ${styles.asplaceholder}`;
   const requiredMark = required ? <span className="text-danger">*</span> : null;
-
   const handleBlur = (e: ChangeEvent<HTMLInputElement>) => {
-    console.log("value:::",value)
-    setFocus(false)
+    console.log("value:::", value);
+    setFocus(false);
     setValue("0");
   };
 
   return (
-    <div
-      className={styles.floatlabel}
-      
-      onFocus={() => setFocus(true)}
-    >
-      <InputNumber onChange={props.onChange} type={type} defaultValue={value} value={dataValue} onBlur={handleBlur}
-      min={props.min}
-      max={props.max}
-
-      formatter={value => `${value}`.padStart(2, '0')}
-      parser={value => value.replace(/^0+/, '')} 
-
-      style ={{ borderColor: "#ccc",
-      borderRadius: "2px",
-      display: "block",
-      height: "4rem",
-      width: "3.5rem",
-      margin: '0.5rem',
-      paddingTop: '1rem'}}/>
+    <div className={styles.floatlabel} onFocus={() => setFocus(true)}>
+      <InputNumber
+        onChange={props.onChange}
+        type={type}
+        defaultValue={parseInt(value, 10)}
+        value={dataValue}
+        onBlur={handleBlur}
+        min={props.min}
+        max={props.max}
+        style={{
+          borderColor: "#ccc",
+          borderRadius: "2px",
+          display: "block",
+          height: "4rem",
+          width: "3.5rem",
+          margin: "0.5rem",
+          paddingTop: "1rem",
+        }}
+      />
 
       <label className={labelClass}>
-
         {isOccupied ? label : placeholder} {requiredMark}
       </label>
     </div>
@@ -62,4 +56,3 @@ const FloatNumberInput: React.FC<FloatNumberInputProps> =(props) => {
 };
 
 export default FloatNumberInput;
-
