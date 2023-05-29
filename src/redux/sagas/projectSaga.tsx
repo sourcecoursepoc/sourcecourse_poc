05c34f2ecd65648b2d90fd7d8d4c902e004e19fd
212040-AnnMariaJenson <212040@ust-global.com>
2023-05-29 13:19:08 +0530

    issue fixing

--- src/redux/sagas/projectSaga.tsx
+++ src/redux/sagas/projectSaga.tsx
@@ -1,22 +1,20 @@
 import axios from "axios";
-import { all, call, put, takeLatest } from "redux-saga/effects";
+import { call, put, take, takeLatest } from "redux-saga/effects";
 
-import {  IPROJECT } from "../actions/types";
-import {  FETCH_ALLPROJECTS_REQUEST} from "../actions/actionTypes";
+import { IPROJECT } from "../actions/types";
+import { FETCH_ALLPROJECTS_REQUEST } from "../actions/actionTypes";
 import { fetchProjectFailure, fetchProjectSuccess } from "../actions/fetchProjectAction";
-import { BASE_URL } from "../../constants/config"
+import { BASE_URL } from "../../constants/config";
 
 const getProjects = () =>
-axios.get<IPROJECT[]>(BASE_URL+"/home");
+  axios.get<IPROJECT[]>(BASE_URL + "/home");
+
 /*
   Worker Saga: Fired on FETCH_TODO_REQUEST action
 */
-function* fetchProjectSaga():any {
-
- 
+function* fetchProjectSaga(): any {
   try {
     const response = yield call(getProjects);
-   
     yield put(
       fetchProjectSuccess({
         projects: response.data,
@@ -32,11 +30,24 @@ function* fetchProjectSaga():any {
 }
 
 /*
-  Starts worker saga on latest dispatched `FETCH_TODO_REQUEST` action.
-  Allows concurrent increments.
+  Starts worker saga on FETCH_ALLPROJECTS_REQUEST action
+  but waits for a take action before actually starting the saga
 */
-function* projectSaga() {
-  yield all([takeLatest(FETCH_ALLPROJECTS_REQUEST, fetchProjectSaga)]);
+function* projectSaga(): any {
+  let isProjectsFetched = false;
+
+  while (true) {
+    // Wait for FETCH_ALLPROJECTS_REQUEST action
+    yield take(FETCH_ALLPROJECTS_REQUEST);
+
+    if (!isProjectsFetched) {
+      yield call(fetchProjectSaga);
+      isProjectsFetched = true;
+    } else {
+      // Projects already fetched, do not make the API call again
+      console.log("Projects already fetched");
+    }
+  }
 }
 
 export default projectSaga;
