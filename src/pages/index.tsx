bb311200f31d4737af81d8e7d67d9bea1e68fa6e
MerinRaj-212052 <212052@ust-global.com>
2023-06-14 15:39:28 +0530

    spin loader implemented in the home page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,48 @@
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
-import { Layout, Space, Col, Row } from "antd";
+import { Layout, Space, Col, Row, Spin } from "antd";
 import Header from "../components/header/header";
 import HomeLeftArea from "../components/homeLeftArea/homeLeftArea";
 import HomeRightArea from "../components/homeRightArea/homeRightArea";
 import styles from "./index.module.css";
 
-import { useEffect } from "react";
-import Schemas from "./schemas";
-
 const Home: React.FunctionComponent = () => {
   const { Content } = Layout;
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulating a delay of 2 seconds for demonstration purposes
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <Space direction="vertical" className={styles.space} size={[0, 48]}>
       <Layout className={styles.layout}>
         <Header />
         <Content>
-          {/* <Schemas></Schemas>  */}
-     <Row>
-            <Col span={16}>
-              <HomeLeftArea />
-            </Col>
-            <Col span={8}>
-              <HomeRightArea />
-            </Col>
-          </Row>  
+          {loading ? (
+            <div className={styles.loader}>
+              <div className={styles.spinnerContainer}>
+                <Spin size="large" className={styles.spinner} />
+              </div>
+            </div>
+          ) : (
+            <>
+              {/* <Schemas /> */}
+              <Row>
+                <Col span={16}>
+                  <HomeLeftArea />
+                </Col>
+                <Col span={8}>
+                  <HomeRightArea />
+                </Col>
+              </Row>
+            </>
+          )}
         </Content>
       </Layout>
     </Space>
